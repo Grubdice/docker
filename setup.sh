e0cd574f32752d85753b506d741c318e784cbe31
Ethan Hall <ethan@ehdev.io>
2014-03-22 11:53:39 -0500

    Fixed the startup

--- setup.sh
+++ setup.sh
@@ -15,8 +15,8 @@ popd
 
 cp services/postgres.service /media/state/units
 sudo systemctl link --runtime /media/state/units/postgres.service
-sudo systemctl start postgres.service
 
 cp services/grubdice.service /media/state/units
 sudo systemctl link --runtime /media/state/units/grubdice.service
-sudo systemctl start grubdice.service
+
+systemctl restart local-enable.service
